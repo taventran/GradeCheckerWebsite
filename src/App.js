8e31ea116724b32067bcef10a90c6e4b287a579b
Taven Tran <taventran@gmail.com>
2022-12-29 00:30:42 -0600

    First push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import FinalsCheckers from './components/finals-checker';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
       </header>
+      <div>
+        {/* <button className="choose">Final Grade Checker</button>
+        <button className="choose">Grade Predictor</button> */}
+      </div>
+      <div className="Finals">
+        <h1>Final Grade Checker</h1>
+        <FinalsCheckers></FinalsCheckers>
+      </div>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/finals-checker.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+
+
+function FinalsChecker() {
+
+    const [ currentGrade, setCurrentGrade ] = useState('');
+    const [desiredGrade, setDesiredGrade ] = useState('');
+    const [ finalWorth, setFinalWorth ] = useState('');
+    const [ gradeRequired, setGradeRequired] = useState(null);
+
+    useEffect(() => {
+        console.log('gradeRequired: ', gradeRequired);
+    }, [gradeRequired]);
+
+    function whenClick() { // Calculates needed score
+        let neededScore = (desiredGrade - currentGrade * (1-finalWorth/100)) / (finalWorth/100);
+        console.log(neededScore);
+        setGradeRequired(neededScore);
+    }
+
+    return (
+        <React.Fragment>
+            <h3>Your current grade</h3>
+            <input placeholder="current grade" value={currentGrade} onChange={evt => setCurrentGrade(evt.target.value)}></input>
+            <h3>Your desired grade</h3>
+            <input placeholder="desired grade" value={desiredGrade} onChange={evt => setDesiredGrade(evt.target.value)}></input>
+            <h3>Your final's worth</h3>
+            <input placeholder="final's worth" value={finalWorth} onChange={evt=> setFinalWorth(evt.target.value)}></input>
+            <br/>
+            <br/>
+            <button onClick={whenClick}>Calculate</button>
+            <h4>Grade Required: {gradeRequired}</h4>
+        </React.Fragment>
+    )
+
+}
+
+export default FinalsChecker;
